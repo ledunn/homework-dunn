ecac01d1f6617878d71665c18d17a0d3842cf4fe
ledunn <laurenelizabeth.dunn@gmail.com>
2015-02-21 09:46:29 -0500

    Updates to Assignment 04

---
+++ mathFruits.js
@@ -0,0 +1,12 @@
+
+/*This is Task 12 for Assignment 04 CMP 3011 (Intermediate Web Design).
+The code below chooses a random piece of fruit from an array of fruit
+in the main HTML file and then prints the random fruit chosen in the text
+of the file.*/
+
+
+
+var randFruits= fruits[Math.floor(Math.random()*fruits.length)];
+        document.write(randFruits);
+
+        
